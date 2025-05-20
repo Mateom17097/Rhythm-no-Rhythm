85675d4ae18b0c112b6904396dda4be5d8cb421b
1917097 <1917097@01-18112.pusd.dom>
2025-05-20 11:21:30 -0700

    WE DID ITTTTTTTTTTTT

---
+++ src/Frame.java
@@ -0,0 +1,126 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.util.concurrent.TimeUnit;
+public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
+	public static boolean debugging = true;
+	//Timer related variables
+	
+	int score = 0;
+	
+	Font myFont = new Font("Courier", Font.BOLD, 40);
+
+//	Music soundBang = new Music("bang.wav", false);
+//	Music soundHaha = new Music("haha.wav", false);
+	
+	public static void main(String[] arg) {
+		Frame f = new Frame();
+		
+	}
+	
+	public int width = 1000;
+	public int height = 1000;
+	
+	public Frame() {
+		JFrame f = new JFrame("Rhythm no Rhythm");
+		f.setSize(new Dimension(width, height));
+		f.setBackground(Color.gray);
+		f.add(this);
+		f.setResizable(false);
+ 		f.addMouseListener(this);
+		f.addKeyListener(this);
+	
+		//backgroundMusic.play();
+		
+	
+		
+		//the cursor image must be outside of the src folder
+		//you will need to import a couple of classes to make it fully 
+		//functional! use eclipse quick-fixes
+		setCursor(Toolkit.getDefaultToolkit().createCustomCursor(
+				new ImageIcon("torch.png").getImage(),
+				new Point(0,0),"custom cursor"));	
+		
+		
+		Timer t = new Timer(16, this);
+		t.start();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+	
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent m) {
+		
+	
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		repaint();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+	
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
