61b72170fd7996d5a1ed4821f5350173c997a98f
shiyambaabu <142447030+shiyambaabu@users.noreply.github.com>
2025-05-29 10:54:08 -0700

    combo stuff

--- src/PinkNote.java
+++ src/PinkNote.java
@@ -8,91 +8,70 @@ import java.awt.geom.AffineTransform;
 import java.net.URL;
 
 public class PinkNote {
-	private Image forward; // backward, left, right; 	
+	private Image forward;
 	private AffineTransform tx;
-	
-	int dir = 0; 					//0-forward, 1-backward, 2-left, 3-right
-	int width, height;				//collision detection
-	int x, y;						//position of the object
-	int vx, vy;						//movement variables
-	double scaleWidth = 0.07;		//change to scale image
-	double scaleHeight = 0.03; 		//change to scale image
+
+	int dir = 0;
+	int width, height;
+	public int x, y;
+	int vx, vy;
+	double scaleWidth = 0.07;
+	double scaleHeight = 0.03;
+
+	public boolean missed = false;
 
 	public PinkNote() {
-		forward 	= getImage("/imgs/"+"Baabu.png"); //load the image for Tree
+		forward = getImage("/imgs/" + "Baabu.png");
 
-		//width and height for hitbox
 		width = 80;
 		height = 80;
-		
-		//used for placement on the JFrame
+
 		x = 1000;
 		y = 500;
-		
-		//if your movement will not be "hopping" base
+
 		vx = -30;
 		vy = 0;
-		
+
 		tx = AffineTransform.getTranslateInstance(0, 0);
-		
-		init(x, y); 				//initialize the location of the image
-									//use your variables
-		
+		init(x, y);
 	}
-	
-	
-	
-	 
+
 	public boolean collided(Hole character) {
-		
-		//represent each object as a rectangle
-		//then check if they are intersecting
 		Rectangle main = new Rectangle(
 			character.getX(),
 			character.getY(),
 			character.getWidth(),
 			character.getWidth()
-			);
-		
+		);
+
 		Rectangle thisObject = new Rectangle(x, y, width, height);
-		
-		//user built-in method to check intersection (collision)
 		return main.intersects(thisObject);
 	}
-	
-	//2nd constructor - allow setting x and  y during construction
-	
-	 
+
 	public void paint(Graphics g) {
-		//these are the 2 lines of code needed draw an image on the screen
 		Graphics2D g2 = (Graphics2D) g;
-		
-		//update x and y if using vx, vy variables
+
 		x += vx;
-		y += vy;	
-		
-		init(x,y);
-		
-		if(x <= 0 ) {
+		y += vy;
+
+		init(x, y);
+
+		if (x <= 0) {
 			x = 920;
-			System.out.println("MISS!");
-			
+			if (!missed) {
+				missed = true;
+				System.out.println("MISS!");
+			}
 		}
-		
-		
-		
+
 		g2.drawImage(forward, tx, null);
-			
-		//draw hitbox based on x,y, width, heigh
-		//for collision detection
-		if(Frame.debugging) {
-			//draw hitbox only if debugging
+
+		if (Frame.debugging) {
 			g.setColor(Color.green);
 			g.drawRect(x, y, width, height);
 		}
-		
 	}
-	
+
 	private void init(double a, double b) {
 		tx.setToTranslation(a, b);
 		tx.scale(scaleWidth, scaleHeight);
@@ -108,5 +87,4 @@ public class PinkNote {
 		}
 		return tempImage;
 	}
-
 }
